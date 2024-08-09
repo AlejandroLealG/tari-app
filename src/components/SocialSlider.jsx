cc64b9817ccc18bd694fd748e5976b776ff78437
AlejandroLealGonzalez <alejandro.leal05@outlook.com>
2024-08-09 11:17:04 -0500

    pequeños arreglos al SocialSlider

--- src/components/SocialSlider.jsx
+++ src/components/SocialSlider.jsx
@@ -1,5 +1,4 @@
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
@@ -10,71 +9,66 @@ import 'swiper/css/scrollbar';
 export default () => {
   return (
     <div className='bg-[#c100b7]'>
-      <div className='flex flex-col items-center justify-center p-8 '>
+      <div className='flex flex-col items-center justify-center p-8'>
         <p className='text-[#fff3e9] text-2xl p-8'>Get a taste for Tari and how to eat differente</p>
         <img src="/images/social.svg" alt="" className='w-[285px] h-[59px]'/>
       </div>
-      <div className='p-12'>
+      <div className='p-12 ml-[80px]'>
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-          spaceBetween={50}
-          slidesPerView={3}
+          slidesPerView={4}  
+          spaceBetween={0}
           loop={true}
-          navigation
-          /*navigation={
-            {
-              nextEl: '.swiper-button-next-custom',
-              prevEl: '.swiper-button-prev-custom',
-            }
-          }*/
-          pagination={{ clickable: true }}
           autoplay={{
             delay: 6000,
             disableOnInteraction: false,
-          }}
+          }} 
         >
-          <SwiperSlide>
+          <SwiperSlide className='flex justify-center items-center'>
             <picture>
               <img
                 src="/images/social1.webp"
                 alt=""
-                className='h-[400px] w-[300px]'
+                className='h-[350px] w-[250px] ' 
               />
             </picture>
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className='flex justify-center items-center'>
             <picture>
               <img
                 src="/images/social2.jpeg"
                 alt=""
+                className='h-[350px] w-[250px] ' 
               />
             </picture>
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className='flex justify-center items-center'>
             <picture>
               <img
                 src="/images/social3.jpeg"
                 alt=""
+                className='h-[350px] w-[250px] ' 
               />
             </picture>
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className='flex justify-center items-center'>
             <picture>
               <img
                 src="/images/social4.jpeg"
                 alt=""
+                className='h-[350px] w-[250px] ' 
               />
             </picture>
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className='flex justify-center items-center'>
             <picture>
               <img
                 src="/images/social5.jpeg"
                 alt=""
+                className='h-[350px] w-[250px] ' 
               />
             </picture>
           </SwiperSlide>
-
         </Swiper>
       </div>
     </div>
